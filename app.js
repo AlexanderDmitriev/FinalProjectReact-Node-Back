5c40fb98d53534b6dc06e260bc8bb2027933cf9d
Kseniia <kseniyamesch0101@gmail.com>
2022-09-25 16:07:28 +0300

    add getAll, add

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const booksRouter = require("./routes/api/books");
+const usersRouter = require("./routes/api/users");
+const statsRouter = require("./routes/api/stats");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/books", booksRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/stats", statsRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
@@ -22,7 +29,3 @@ app.use((req, res) => {
   });
   
 module.exports = app;
-
-// app.listen(3001, () => 
-//     console.log("Web-server success running")
-// )
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const RequestError = require("./RequestError");
+const handleSchemaValidationErrors = require("./handleSchemaValidationErrors");
 
 module.exports = {
     ctrlWrapper,
-    RequestError
+    RequestError,
+    handleSchemaValidationErrors
 }
