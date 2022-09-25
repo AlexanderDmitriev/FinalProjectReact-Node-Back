b74df56aa88a78bed726604efd4fb50c57be91f1
nestakiev <n3st1k13@gmail.com>
2022-09-25 18:37:17 +0300

    swagger v1 done
    training version

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require ("express");
 const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +14,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
