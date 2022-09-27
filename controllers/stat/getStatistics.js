a8413a8b3ae6318fa7cb85817e107b0713ae1d87
nestakiev <n3st1k13@gmail.com>
2022-09-27 17:46:23 +0300

    add DailyStatistics part

--- controllers/stat/getStatistics.js
+++ controllers/stat/getStatistics.js
@@ -4,7 +4,7 @@ const { RequestError } = require("../../helpers");
 const getStatistics = async (req, res) => {
     const { _id } = req.user;
 
-    const result = Stat.findOne({owner: _id})
+    const result = await Stat.findOne({owner: _id})
 
     if (!result) {
         throw RequestError(500, "Something went wrong");
