1702a277a4c69cf4ae64d92ff070331a561aca9d
AlexanderDmitriev <alex9110077@gmail.com>
2022-09-28 01:03:06 +0200

    Update books.js

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,23 +2,23 @@ const express = require("express");
 
 const ctrl = require("../../controllers/books");
 
-const {ctrlWrapper} = require("../../helpers");
+const { ctrlWrapper } = require("../../helpers");
 
-const { validationBody, isValidId } = require("../../middlewares")
+const { validationBody, isValidId } = require("../../middlewares");
 
-const {schemas} = require("../../models/book");
+const { schemas } = require("../../models/book");
 
 const router = express.Router();
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.post("/", validationBody(schemas.addSchema) , ctrlWrapper(ctrl.add));
+router.post("/", validationBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
 router.patch(
-    "/:bookId",
-    isValidId,
-    validationBody(schemas.updateResumeSchema),
-    ctrlWrapper(ctrl.updateResume)
-  );
+  "/:bookId",
+  isValidId,
+  validationBody(schemas.updateResumeSchema),
+  ctrlWrapper(ctrl.updateResume)
+);
 
 module.exports = router;
