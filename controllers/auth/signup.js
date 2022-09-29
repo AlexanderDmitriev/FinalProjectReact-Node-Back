c97680739e0b4a2ea29013ccbb8ea22e35728e33
AlexanderDmitriev <alex9110077@gmail.com>
2022-09-29 22:03:57 +0200

    Update signup.js

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -12,6 +12,7 @@ const signup = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 10);
     const result = await User.create({ name, email, password: hashPassword });
     res.status(201).json({
+        name: result.name,
         email: result.email,
     });
 };
