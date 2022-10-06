fe01b88d837946bde8ebf064dc2c53f20b41b81f
Daemon-L <85022413+Daemon-L@users.noreply.github.com>
2022-10-07 00:49:09 +0300

    googleEnv (#47)

--- models/user.js
+++ models/user.js
@@ -1,60 +1,69 @@
-const { Schema, model } = require('mongoose');
-const Joi = require('joi');
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
 const { handleSchemaValidationErrors } = require("../helpers");
 
-// const emailRegexp = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
-const emailRegexp = /^[\w.]+@[\w]+.[\w]+$/;
+const emailRegexp =
+  /^(([a-z0-9_-]+\.)*[a-z0-9_-]{2,6})+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
 
-const userShema = new Schema({
-  name: {
-  type: String,
-  required: [true, "Set name for user"],
+const passwordRegexp = /^[^-\.А-Яа-я ][^А-Яа-я ]{4,29}$/;
+
+const nameRegexp =
+  /^(([a-zA-Z0-9_' -]{3,100})|([а-яА-ЯЁёІіЇїҐґЄє0-9_' -]{3,100}))$/u;
+
+const userShema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: 3,
+      maxLength: 100,
+      required: [true, "Set name for user"],
+    },
+    email: {
+      type: String,
+      minlength: 6,
+      maxLength: 63,
+      required: [true, "Email is required"],
+      match: emailRegexp,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 5,
+      required: [true, "Password is required"],
+    },
+    token: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    minlength: 6,
-    maxLength: 63,
-    required: [true, 'Email is required'],
-    match: emailRegexp,
-    unique: true,
-  },   
-  password: {
-    type: String,
-    minlength: 6,  
-    required: [true, 'Password is required'],
-  },
-  token: {
-    type: String,
-    default: "",
-  },
-}, { versionKey: false, timestamps: true });
+  { versionKey: false, timestamps: true }
+);
 
 userShema.post("save", handleSchemaValidationErrors);
 
-
 // ** Joi schemas ***************************************
 const singupSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().pattern(emailRegexp).min(6).required(),
-  password: Joi.string().min(6).max(63).required(), 
-  repeat_password: Joi.string().required().valid(Joi.ref('password')),
-})
+  name: Joi.string().pattern(nameRegexp).min(3).max(100).required(),
+  email: Joi.string().min(6).max(63).required(),
+  password: Joi.string().pattern(passwordRegexp).min(5).max(30).required(),
+  repeat_password: Joi.string().required().valid(Joi.ref("password")),
+});
 
 const loginSchema = Joi.object({
-  email: Joi.string().min(6).max(63).pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
-})
+  email: Joi.string().min(6).max(63).required(),
+  password: Joi.string().pattern(passwordRegexp).min(5).max(30).required(),
+});
 
 const schemas = {
   singupSchema,
   loginSchema,
-}
+};
 // *****************************************************
 
 const User = model("user", userShema);
 
 module.exports = {
-    User,
-    schemas,
-}
+  User,
+  schemas,
+};
--- routes/google/auth_controller.js
+++ routes/google/auth_controller.js
@@ -1,76 +1,73 @@
-const queryString = require('query-string');
-const axios = require('axios');
-const { v4 } = require('uuid');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const { User } = require('../../models/user');
+const queryString = require("query-string");
+const axios = require("axios");
+const { v4 } = require("uuid");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models/user");
 const { SECRET_KEY } = process.env;
 
 // GOOGLE ************************************************
 const googleAuth = async (req, res) => {
-    const stringifiedParams = queryString.stringify({
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        redirect_uri: `${process.env.BASE_URL}/api/users/google-redirect`,
-        scope: [
-            "https://www.googleapis.com/auth/userinfo.email",
-            "https://www.googleapis.com/auth/userinfo.profile",
-        ].join(" "),
-        response_type: "code",
-        access_type: "offline",
-        prompt: "consent"
-    });
-    return res.redirect(
-        `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
-    )
+  const stringifiedParams = queryString.stringify({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: `${process.env.BASE_URL}/api/users/google-redirect`,
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+  });
+  return res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+  );
 };
 
 const googleRedirect = async (req, res) => {
-    const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-    const urlObj = new URL(fullUrl);
-    const urlParams = queryString.parse(urlObj.search);
-    const code = urlParams.code;
-    const tokenData = await axios({
-        url: `https://oauth2.googleapis.com/token`,
-        method: "post",
-        data: {
-            client_id: process.env.GOOGLE_CLIENT_ID,
-            client_secret: process.env.GOOGLE_CLIENT_SECRET,
-            redirect_uri: `${process.env.BASE_URL}/api/users/google-redirect`,
-            grant_type: "authorization_code",
-            code,
-        },
-    });
-    const userData = await axios({
-        url: "https://www.googleapis.com/oauth2/v2/userinfo",
-        method: "get",
-        headers: { Authorization: `Bearer ${tokenData.data.access_token}`},
-    });
-    // GOOGLE END********************************************
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search);
+  const code = urlParams.code;
+  const tokenData = await axios({
+    url: `https://oauth2.googleapis.com/token`,
+    method: "post",
+    data: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${process.env.BASE_URL}/api/users/google-redirect`,
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: { Authorization: `Bearer ${tokenData.data.access_token}` },
+  });
+  // *****************************************************
 
-    // SINGUP *********************************************
-    const email = userData.data.email;
-    const name = userData.data.name;
-    let user = await User.findOne({ email });
-    if (!user) {
-        const password = v4();
-        const hashPassword = await bcrypt.hash(password, 10);
-        user = await User.create({ name, email, password: hashPassword });
-        // console.log(`User was created`)
-    }
-    //  ***************************************************
+  // SINGUP *********************************************
+  const email = userData.data.email;
+  const name = userData.data.name;
+  let user = await User.findOne({ email });
+  if (!user) {
+    const password = v4();
+    const hashPassword = await bcrypt.hash(password, 10);
+    user = await User.create({ name, email, password: hashPassword });
+  }
+  //  ***************************************************
 
-    //  LOGIN *********************************************
-    const peyload = { id: user._id };
-    const token = jwt.sign(peyload, SECRET_KEY, { expiresIn: "24h" });
-    await User.findByIdAndUpdate(user._id, { token });
-    // console.log('Login is successful');
-    //  ***************************************************
-    return res.redirect(
-        `http://localhost:3001/?token=${token}&name=${name}&email=${email}`
-    )
-}
+  //  LOGIN *********************************************
+  const peyload = { id: user._id };
+  const token = jwt.sign(peyload, SECRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  //  ***************************************************
+  return res.redirect(
+    `http://${process.env.FRONT_URL}/?token=${token}&name=${name}&email=${email}`
+  );
+};
 module.exports = {
-    googleAuth,
-    googleRedirect,
+  googleAuth,
+  googleRedirect,
 };
