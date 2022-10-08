23efc230002c99a65c7379c4a70c9e5a30675f24
AlexanderDmitriev <alex9110077@gmail.com>
2022-10-08 18:49:17 +0200

    Update auth_controller.js

--- routes/google/auth_controller.js
+++ routes/google/auth_controller.js
@@ -19,14 +19,14 @@ const googleAuth = async (req, res) => {
     access_type: "offline",
     prompt: "consent",
   });
-  // console.log("1111111111111111111111");
+  console.log("1111111111111111111111");
   return res.redirect(
     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
   );
 };
 
 const googleRedirect = async (req, res) => {
-  // console.log("22222222222222222222");
+  console.log("22222222222222222222");
   const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
   const urlObj = new URL(fullUrl);
   const urlParams = queryString.parse(urlObj.search);
@@ -37,7 +37,7 @@ const googleRedirect = async (req, res) => {
     data: {
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.BASE_URL}/api/users/google-redirect`,
+      redirect_uri: `${process.env.BASE_URL}api/users/google-redirect`,
       grant_type: "authorization_code",
       code,
     },
